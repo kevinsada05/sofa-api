714ffe17e8eadf4ea27948bd0de708984f1a88e5
kevinsada05 <kevinsada05@gmail.com>
2025-10-03 03:41:18 +0200

    wip

--- app/Http/Controllers/Api/DashboardController.php
+++ app/Http/Controllers/Api/DashboardController.php
@@ -9,7 +9,13 @@ class DashboardController extends Controller
 {
     public function me(Request $request)
     {
-        $user = $request->user()->load('profile', 'type');
+        $user = $request->user();
+
+        if (!$user) {
+            return response()->json(['message' => 'Unauthenticated'], 401);
+        }
+
         return response()->json($user);
     }
+
 }
